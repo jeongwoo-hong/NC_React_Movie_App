11d04fb933559292a2ab3c0e8f1bc25ca00aec57
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-26 23:38:20 +0900

    22.12.26.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+function Food() {
+  return <h1> I like Potato </h1>
+}
+
 function App() {
   return (
   <div>
     <h1>Hello</h1>
+    <Food name="kimchi" />
   </div>
   )}
 
--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function Potato(){
-    return (
-        <h3>I love poatato</h3>
-    )
-}
-
-export default Potato
