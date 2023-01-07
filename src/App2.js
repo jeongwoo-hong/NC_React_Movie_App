579f2c5bc9ab45aef3dff0211cf05c5490e4c2b6
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-01-07 18:03:07 +0900

    23.1.7.2

--- src/App2.js
+++ src/App2.js
@@ -1,8 +1,25 @@
 import React from "react";
-import PropTypes from "prop=types";
+// import PropTypes from "prop-types";
 
-class App2 extend React.Component{
-
+class App2 extends React.Component{
+    state = {
+        count: 0
+    }
+    add = () => {
+        console.log("add")
+    }
+    minus = () => {
+        console.log("minus")
+    }
+    render() {
+        return (
+            <div>
+                <h1>The number is :{this.state.count}</h1>
+                <button onClick={this.add}>Add</button> 
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        ) 
+    }
 
 }
 
