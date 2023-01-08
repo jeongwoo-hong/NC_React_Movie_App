69b2438185416087ed6290cdaf534246cd3627bf
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-01-08 21:19:41 +0900

    23.1.8.1

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App2 from './App2';
 
-ReactDOM.render(<App2 />, document.getElementById("root"));
+ReactDOM.render(<App2/>, document.getElementById("root"));
