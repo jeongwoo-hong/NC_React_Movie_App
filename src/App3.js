59d8c35f9b2fe39dc74a1f33640309d4b22a235d
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-01-12 02:02:18 +0900

    23.1.12.4

--- src/App3.js
+++ src/App3.js
@@ -1,10 +1,15 @@
 import React from "react";
-// import PropTypes from "prop-types";
 
 class App3 extends React.Component{
+    state = {
+        isLoading: true
+    }
     render() {
+        const { isLoading } = this.state;
         return (
-        
+            <div>
+                {isLoading ? "Loading" : "We are ready"}
+            </div>
         ) 
     }
 
