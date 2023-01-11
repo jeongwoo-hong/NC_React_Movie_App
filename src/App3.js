d35f6b20e5c2097e9bb63c6145f84d3e9f8d5d50
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-01-12 01:50:43 +0900

    23.1.12.4

---
+++ src/App3.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+class App3 extends React.Component{
+    render() {
+        return (
+        
+        ) 
+    }
+
+}
+
+export default App3;
