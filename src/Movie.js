a8987bb187010e2b05a4abc8441cc765ddc20fb2
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-01-18 05:07:34 +0900

    23.1.18.10

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,11 @@ import React from "react";
 import PropTypes from "prop-types"
 
 function Movie({id, year, title, summary, poster}) {
-    return <h1>{title}</h1>
+    return <div class="movies_movie">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <p class="movie_summary">{summary}</p>
+    </div>
 }
 
 // eslint-disable-next-line react/no-typos
