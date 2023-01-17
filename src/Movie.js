edd08f6207fa5459af88f2ee3936b03d15a53287
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-01-18 05:10:14 +0900

    23.1.18.11

--- src/Movie.js
+++ src/Movie.js
@@ -3,9 +3,12 @@ import PropTypes from "prop-types"
 
 function Movie({id, year, title, summary, poster}) {
     return <div class="movies_movie">
-        <h3 class="movie_title">{title}</h3>
-        <h5 class="movie_year">{year}</h5>
-        <p class="movie_summary">{summary}</p>
+        <img src={poster} alt={title} title={title} />
+        <div class="movie_data">
+            <h3 class="movie_title">{title}</h3>
+            <h5 class="movie_year">{year}</h5>
+            <p class="movie_summary">{summary}</p>
+        </div>
     </div>
 }
 
