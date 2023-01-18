5d38e1314a2463235764490f0ecfe8e0af33cefa
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-01-19 01:35:47 +0900

    23.1.19.5

--- src/App3.js
+++ src/App3.js
@@ -2,16 +2,17 @@ import React from "react";
 import {HashRouter, Route} from "react-router-dom"
 import About from "./routes/About"
 import Home from "./routes/Home"
+import Navigation from "./component/Navigation";
+import "./App3.css"
 
 function App3() {
-    return <HashRouter>
-        <Route path="/">
-            <h1>Home</h1>
-        </Route>
-        <Route path="/about">
-            <h1>About</h1>
-        </Route>
+    return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
     </HashRouter>
+    )
 }
 
 export default App3
